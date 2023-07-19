b4e32c46a7adbe020a1630e602e44172f7d3ec8e
3zHrb <zezo.hoooh@gmail.com>
2023-07-20 01:36:21 +0300

    website is done and reday to be deployed v0.1

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__, static_folder="statics", template_folder="templates")
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
